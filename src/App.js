ab72144f0e9ac50a868578c15203fb5847ada16b
Eric Masi <eric@gitmasi.com>
2019-04-21 07:04:38 -0400

    add coin pictures, add coin component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Coin from './components/Coin';
 
 class App extends Component {
   render() {
     return (
-
+      <Coin/>
     );
   }
 }
---
+++ src/components/Coin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Coin.css';
+import heads from '../assets/images/one-hundred-yen-coin-heads-removebg.png';
+import tails from '../assets/images/one-hundred-yen-coin-tails-removebg.png';
+
+const Coin = props => {
+  return (
+    <div className="Coin">
+      <img src={heads} alt="Japanese 100 yen coin"/>
+    </div>
+  )
+}
+
+export default Coin;
